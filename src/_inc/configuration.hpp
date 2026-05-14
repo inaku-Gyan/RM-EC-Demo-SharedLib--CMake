666b4f6e34533780f226dfd13fb97d0e4c1b0ef7
Gyan <inaku.gs@gmail.com>
2026-05-15 06:25:56 +0800

    init config

---
+++ src/_inc/configuration.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "ecx_config_default.hpp"  // IWYU pragma: export
+
+#if __has_include(<ecx_config.hpp>)
+  #include <ecx_config.hpp>  // IWYU pragma: export
+#endif
+
+#ifndef ECX_USE_DEBUG
+  #define ECX_USE_DEBUG 0
+#endif
+
+#if ECX_USE_DEBUG && !defined(ECX_ASSERT)
+  #include <cassert>
+  #define ECX_ASSERT(expr) assert(expr)
+#elif !defined(ECX_DEBUG)
+  #define ECX_ASSERT(expr) (static_cast<void>(0))
+#endif
---
+++ src/_inc/ecx_config_default.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+// TODO: 正式发布时，将此去除
+#define ECX_USE_DEBUG 1
+
+#define ECX_USE_HAL 1
+#define ECX_INC_HAL <stm32f4xx_hal.h>
+
+#define ECX_USE_CMSIS_RTOS2 1
+#define ECX_INC_CMSIS_RTOS2 <cmsis_os2.h>
