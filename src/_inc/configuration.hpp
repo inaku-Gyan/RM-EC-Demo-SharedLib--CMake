878f0a36f49a9ae1f7226f5f957d5300a1a7855d
Gyan <inaku.gs@gmail.com>
2026-05-15 22:42:40 +0800

    fix: 更新 ECX 配置以启用开发调试和用户检查功能

--- src/_inc/configuration.hpp
+++ src/_inc/configuration.hpp
@@ -7,13 +7,28 @@
   #include <ecx_config.hpp>  // IWYU pragma: export
 #endif
 
-#ifndef ECX_USE_DEBUG
-  #define ECX_USE_DEBUG 0
+//// ECX 库内部使用的断言（仅在开发 ECX 库时启用） ////
+
+#ifndef ECX_USE_DEV_DEBUG
+  #define ECX_USE_DEV_DEBUG 0
 #endif
 
-#if ECX_USE_DEBUG && !defined(ECX_ASSERT)
+#if ECX_USE_DEV_DEBUG && !defined(ECX_DEV_ASSERT)
+  #include <cassert>
+  #define ECX_DEV_ASSERT(expr) assert(expr)
+#elif ECX_USE_DEV_DEBUG == 0
+  #define ECX_DEV_ASSERT(expr) (static_cast<void>(0))
+#endif
+
+//// 用户使用 ECX 时的断言（检查用户对 ECX 的使用是否正确） ////
+
+#ifndef ECX_USE_CHECKS
+  #define ECX_USE_CHECKS 0
+#endif
+
+#if ECX_USE_CHECKS && !defined(ECX_ASSERT)
   #include <cassert>
   #define ECX_ASSERT(expr) assert(expr)
-#elif !defined(ECX_DEBUG)
+#elif ECX_USE_CHECKS == 0
   #define ECX_ASSERT(expr) (static_cast<void>(0))
 #endif
--- src/_inc/ecx_config_default.hpp
+++ src/_inc/ecx_config_default.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
 // TODO: 正式发布时，将此去除
-#define ECX_USE_DEBUG 1
+#define ECX_USE_DEV_DEBUG 1  // 检查 ECX 的代码是否正确（用于开发 ECX 库本身）
+#define ECX_USE_CHECKS 1     // 检查用户对 ECX 的使用是否正确
 
 #define ECX_USE_HAL 1
 #define ECX_INC_HAL <stm32f4xx_hal.h>
