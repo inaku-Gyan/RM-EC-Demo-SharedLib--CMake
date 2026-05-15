d872c8b37eda71d8b8e205cb790bf18f8504d318
Gyan <inaku.gs@gmail.com>
2026-05-16 07:29:30 +0800

    fix: 禁用开发调试和使用断言宏以准备正式发布

--- src/_inc/ecx_config_default.hpp
+++ src/_inc/ecx_config_default.hpp
@@ -1,8 +1,7 @@
 #pragma once
 
-// TODO: 正式发布时，将此去除
-#define ECX_USE_DEV_DEBUG 1     // 检查 ECX 的代码是否正确（用于开发 ECX 库本身）
-#define ECX_USE_USAGE_ASSERT 1  // 检查用户对 ECX 的使用是否正确
+#define ECX_USE_DEV_DEBUG 0     // 检查 ECX 的代码是否正确（用于开发 ECX 库本身）
+#define ECX_USE_USAGE_ASSERT 0  // 检查用户对 ECX 的使用是否正确
 
 // #define ECX_INC_CMSIS_DEVICE_HEADER "stm32f4xx.h"
 #define ECX_INC_HAL "stm32f4xx_hal.h"
