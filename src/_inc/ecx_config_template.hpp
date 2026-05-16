0b29722b82b703edb3740057fd6acd59ef7a39ab
Gyan <inaku.gs@gmail.com>
2026-05-16 12:25:45 +0800

    feat: 添加用户配置模板

---
+++ src/_inc/ecx_config_template.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+// 检查用户对 ECX 的使用是否正确
+#define ECX_USE_USAGE_CHECK 1
+
+// 检查 ECX 的代码是否正确（用于开发 ECX 库本身）
+#define ECX_USE_DEV_DEBUG 1
+
+// #define ECX_INC_CMSIS_DEVICE_HEADER "stm32f4xx.h"
+
+#define ECX_INC_HAL "stm32f4xx_hal.h"
+
+#define ECX_INC_CMSIS_RTOS2 "cmsis_os2.h"
