1c66e17cc5524b2c9d4df908741394f044af2fd0
Gyan <inaku.gs@gmail.com>
2026-05-16 21:30:48 +0800

    feat: 引入用户配置和开发调试配置的相关宏定义

--- src/_inc/ecx_config_template.hpp
+++ src/_inc/ecx_config_template.hpp
@@ -11,3 +11,9 @@
 #define ECX_INC_HAL "stm32f4xx_hal.h"
 
 #define ECX_INC_CMSIS_RTOS2 "cmsis_os2.h"
+
+// 如果通过其他方式（比如编译指令等）手动引入了该配置文件，则可以定义如下宏来避免重复引入。
+// 当然，重复引入一般不会导致问题，因为有头文件守卫。
+// 此开关预留于可能的特殊情况。
+// 如果是用 ECX 的自动机制引入用户配置，则在此处定义该宏不会有任何效果。
+// #define ECX_USER_CONFIG_INCLUDED 1
