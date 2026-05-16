bc556ddc0eb4177726fc1f71c5c3990a5092e965
Gyan <inaku.gs@gmail.com>
2026-05-16 10:56:41 +0800

    默认 config 改为在用户配置之后引入

--- src/_inc/configuration.hpp
+++ src/_inc/configuration.hpp
@@ -1,12 +1,10 @@
 #pragma once
 
-#ifndef ECX_NO_DEFAULT_CONFIG
-  #include "ecx_config_default.hpp"  // IWYU pragma: export
-#endif
-
 #ifndef ECX_INC_USER_CONFIG
   #define ECX_INC_USER_CONFIG "ecx_config.hpp"
 #endif
+
+#include "ecx_config_default.hpp"  // IWYU pragma: export
 
 // 让用户配置覆盖默认配置
 #if __has_include(ECX_INC_USER_CONFIG)
@@ -14,10 +12,6 @@
 #endif
 
 //// ECX 库内部使用的断言（仅在开发 ECX 库时启用） ////
-
-#ifndef ECX_USE_DEV_DEBUG
-  #define ECX_USE_DEV_DEBUG 0
-#endif
 
 #if ECX_USE_DEV_DEBUG && !defined(ECX_DEV_ASSERT)
   #include <cassert>
@@ -27,10 +21,6 @@
 #endif
 
 //// 用户使用 ECX 时的断言（检查用户对 ECX 的使用是否正确） ////
-
-#ifndef ECX_USE_USAGE_CHECK
-  #define ECX_USE_USAGE_CHECK 0
-#endif
 
 #if ECX_USE_USAGE_CHECK && !defined(ECX_USAGE_ASSERT)
   #include <cassert>
